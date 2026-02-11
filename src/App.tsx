1940a01f7f2624182c4d10f265c3122379424bcf
realth000 <a598919335@gmail.com>
2026-02-11 16:47:43 +0800

    feat: direct access pages with hash and anchor
    This commit added direct access to pages with hash and anchor.
    
    In `HOST/#/foo/bar#baz`, where:
    
    * `/foo/bar` is `hash` (page route) to find pages.
    * `baz` is `anchor` to specify a certain section in pages.
      * `baz` is the target element id in DOM.
    * `/foo/bar#baz` is the "hash" recognized by browser, mixed `hash` and
      `anchor`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,78 @@
-import {
-  createContext, createElement, useEffect, useEffectEvent, useRef, useState,
-} from 'react'
+import { createElement, useEffect, useEffectEvent, useRef, useState } from 'react'
 import { findRoute, notFondPageTarget } from '@/router/router.ts'
 import { log } from './log.ts'
 import { purifyUrl } from './utils/encoding.ts'
 
 
 /**
- * The global route context.
+ *
+ * Split full hash '/foo/bar#baz' into:
+ *
+ * * hash: '/foo/bar/'
+ * * anchor: 'baz'
  */
-const RouteContext = createContext('')
+function splitHashAndAnchor(data: string): { hash: string, anchor: string | undefined } {
+  const s = data.indexOf('#')
+  return s == -1 ? { hash: data, anchor: undefined } : { hash: data.slice(0, s), anchor: data.slice(s+1) }
+}
 
 export default function App() {
-  const [currentPage, setCurrentPage] = useState('/')
-  const prevHash = useRef('')
+  /**
+   * The hash here is the part of hash for routing.
+   */
+  const [currHash, setCurrHash] = useState<string | undefined>()
+
+  /**
+   * Last anchor in url.
+   */
+  const [currAnchor, setCurrAnchor] = useState<string | undefined>()
+
+  /**
+   * Concated hash and anchor forms the entire url hash.
+   */
+  const prevHashAndAnchor = useRef('')
 
   const renderPage = () => {
     const path = globalThis.location.pathname
-    const hash = globalThis.location.hash.slice(1) || '/'
-    log(`renderPage: path=${path}, hash=${hash}`)
-    return createElement((findRoute(currentPage) ?? notFondPageTarget).component)
+    const { hash, anchor } = splitHashAndAnchor(globalThis.location.hash.slice(1) || '/')
+    const targetRoute = currHash ?? hash
+    if (currAnchor !== anchor) {
+      setCurrAnchor(anchor)
+    }
+
+    log(`renderPage: currnetPage=${currHash ?? '<undefined>'}, path=${path}, hash=${hash}, anchor=${anchor ?? '<undefined>'}`)
+    return createElement((findRoute(targetRoute) ?? notFondPageTarget).component)
   }
 
+  const onScrollToElementRequired = useEffectEvent((hash: string) => {
+    // Only scroll into view by id
+    //
+    // When user clicked links with href `#foo` then scroll to foo,
+    // where foo is already in the page, just do the scrolling.
+
+    let newHash: string
+    if (prevHashAndAnchor.current.length === 0) {
+      // No current anchor indicates user is accessing with direct url to some page.
+      const { hash: currHash } = splitHashAndAnchor(globalThis.location.hash.slice(1) || '/')
+      newHash = `${currHash}#${hash}`
+    } else {
+      const prevHashAnchorIndex = prevHashAndAnchor.current.indexOf('#')
+      newHash = prevHashAnchorIndex === -1 ? `${prevHashAndAnchor.current}#${hash}` : `${prevHashAndAnchor.current.slice(0, prevHashAnchorIndex)}#${hash}`
+    }
+
+    log('replace state with', `/#${newHash}`, 'prevHashAndAnchor', prevHashAndAnchor)
+    globalThis.history.replaceState({}, '', `/#${newHash}`)
+
+    const elementId = '#' + hash
+    const targetElement = document.querySelector(elementId)
+    if (!targetElement) {
+      log(`failed to scroll to element with hash id ${elementId}: element not found`)
+      return
+    }
+
+    targetElement.scrollIntoView()
+    prevHashAndAnchor.current = newHash
+  })
 
   /**
    * The callback handler when hash section in the current url changed.
@@ -39,30 +90,19 @@ export default function App() {
       //
       // where /foo/bar is the route and baz is the anchor to scroll.
       // The scrolling is triggered after `currentPage` is updated.
-      setCurrentPage(hash)
-      prevHash.current = hash
+      setCurrHash(hash)
+      prevHashAndAnchor.current = hash
     } else {
-      // Only scroll into view by id
-      //
-      // This branch is possible as the user clicked links with href `#foo` to scroll to foo
-      // where foo is already in the page, just do the scrolling.
-
-      const prevHashAnchorIndex = prevHash.current.indexOf('#')
-      const newHash = prevHashAnchorIndex === -1 ? `${prevHash.current}#${hash}` : `${prevHash.current.slice(0, prevHashAnchorIndex)}#${hash}`
-      globalThis.history.replaceState({}, '', `/#${newHash}`)
-
-      const elementId = '#' + hash
-      const targetElement = document.querySelector(elementId)
-      if (!targetElement) {
-        log(`failed to scroll to element with hash id ${elementId}: element not found`)
-        return
-      }
-
-      targetElement.scrollIntoView()
-      prevHash.current = newHash
+      // Scroll in current page, to a specified element.
+      onScrollToElementRequired(hash)
     }
   })
 
+  /**
+   * Handle url hash changes.
+   *
+   * Here the hash is raw hash in url, including hash (for routing) and anchor (for deep linking).
+   */
   useEffect(() => {
     globalThis.addEventListener('hashchange', onHashChanged)
 
@@ -71,14 +111,20 @@ export default function App() {
     }
   }, [])
 
+  /**
+   * Handle anchor changes.
+   *
+   * Anchor changed means need to scroll to current element.
+   */
+  useEffect(() => {
+    if (currAnchor) {
+      onScrollToElementRequired(currAnchor)
+    }
+  }, [currAnchor])
+
   return (
-    <RouteContext value='/'>
+    <>
       {renderPage()}
-    </RouteContext>
+    </>
   )
 }
-
-// OnHashChanged()
-
-// renderPage(currentPage)
-
