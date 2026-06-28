c057aca4b9e3d52c3cfd8bd208b41b7622b1135d
realth000 <a598919335@gmail.com>
2026-06-29 03:28:37 +0800

    fix: remove unused variable

--- scripts/highlighter/languages.ts
+++ scripts/highlighter/languages.ts
@@ -14,31 +14,6 @@ const languages: LanguageConfig[] = [
   dartConfig,
   rustConfig,
 ]
-// prettier-ignore
-const _langKeywords = new Map<string, string[]>([
-  [
-    'cmake',
-    [
-      'if', 'else', 'elseif', 'endif', 'foreach', 'endforeach', 'while', 'endwhile', 'macro', 'endmacro', 'function',
-      'endfunction', 'return', 'block', 'endblock', 'break', 'continue', 'set', 'unset', 'option', 'cmake_minimum_required',
-      'project', 'add_executable', 'add_library', 'target_link_libraries', 'target_include_directories', 'target_compile_definitions',
-      'target_compile_options', 'target_sources', 'find_package', 'find_library', 'find_path', 'find_file', 'find_program', 'include',
-      'add_subdirectory', 'configure_file', 'file', 'string', 'list', 'math', 'message', 'execute_process', 'include_directories',
-      'link_directories', 'add_definitions', 'compile_definitions', 'install', 'enable_testing', 'add_test', 'set_target_properties',
-      'get_target_property', 'set_property', 'get_property', 'target_compile_features', 'add_custom_command', 'add_custom_target',
-      'add_dependencies', 'aux_source_directory', 'define_property', 'mark_as_advanced', 'site_name', 'variable_watch', 'PUBLIC',
-      'PRIVATE', 'INTERFACE', 'KEYWORDS', 'COMPONENTS', 'REQUIRED', 'OPTIONAL', 'MODULE', 'SHARED', 'STATIC', 'OBJECT', 'INTERFACE',
-      'EXCLUDE_FROM_ALL', 'SYSTEM', 'BEFORE', 'AFTER', 'STATUS', 'WARNING', 'AUTHOR_WARNING', 'SEND_ERROR', 'FATAL_ERROR', 'DEPRECATION',
-    ],
-  ],
-  [
-    'bash',
-    [
-      'if', 'fi', 'case', 'esac', 'while', 'when', 'do', 'done', 'then', 'return', 'exit', 'echo', '>&', '$', '$(', ')', '(', '}', '{'
-    ]
-  ],
-]
-)
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
